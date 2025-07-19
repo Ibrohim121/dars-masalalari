d70c1630d4c4438e2a60f08d3e3c21af508c48a6
Ibrohim121 <igofurjonov229@gmail.com>
2025-07-19 15:39:11 +0500

    darsdagi normativ1

---
+++ script.js
@@ -0,0 +1,80 @@
+
+//1ch masala
+// let sonlar = +prompt() 
+// if (sonlar % 2 == 0) {
+//     alert("Sonlar juft")}
+//     else {
+//         alert ("Sonlar toq")}
+
+// console.log(sonlar)
+
+//2-masala
+// let sonlar = +prompt() 
+// if ( sonlar === 0) {
+//     alert("sonlar 0 ga teng")
+// }
+// if (sonlar % 2 == 0) {
+//     alert("Sonlar juft")}
+//     else {
+//         alert ("Sonlar toq")}
+
+// console.log(sonlar)
+
+//3-masala 
+
+// function sonlar(son, son1) {
+//     if (son > son1) {
+//         alert("1-son katta")
+//     } else if(son < son1){
+//         alert("2-son katta")
+//     }else {
+//         alert("sonlar teng")
+//     }
+
+
+
+
+// }
+
+// sonlar(8, 8)
+
+//4-masaala
+// function baholar(baho) {
+//     if (baho <= 59) {
+//         alert("imtihondan otmadi baho 2")
+//     } else if(59 <baho <= 69){
+//         alert("imtihondan otdi baho 3")
+//     }else if (70<baho<=89){
+//         alert("imtihondan otdi baho 4")
+
+//     }else if (90<baho<=100) {
+//         alert("imtihondan otdi 5")
+//     }
+
+// }
+// baholar(55)
+
+
+// 5- masala
+
+
+// 5-savol
+// function tekshiruv() {
+//     const togriLogin = "ustudy2025";
+//     const togriParol = "U9frontend";
+
+//     const login = prompt("Loginni kiriting:");
+//     const parol = prompt("Parolni kiriting:");
+
+//     if (login === togriLogin && parol === togriParol) {
+//         console.log("Muvaffaqiyatli");
+//     } else if (login !== togriLogin && parol === togriParol) {
+//         console.log("Kiritilgan login xato");
+//     } else if (login === togriLogin && parol !== togriParol) {
+//         console.log("Kiritilgan password xato");
+//     } else {
+//         console.log("Kiritilgan login va password xato");
+//     }
+// }
+
+// tekshiruv();
